5cc0f673f924024c348d990b42b0fcbfa1b405c2
codygriffith <cody@alenthea.com>
2021-03-28 06:33:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 async function register ({
   registerHook,
-  registerSetting,
-  settingsManager,
-  storageManager,
-  videoCategoryManager,
-  videoLicenceManager,
-  videoLanguageManager
+  storageManager
 }) {
+  const fieldName = 'my-field-name'
 
-}
+  // Store data associated to this video
+  registerHook({
+    target: 'action:api.video.updated',
+    handler: ({ video, body }) => {
+      if (!body.pluginData) return
 
-async function unregister () {
-  return
-}
+      const value = body.pluginData[fieldName]
+      if (!value) return
 
-module.exports = {
-  register,
-  unregister
+      storageManager.storeData(fieldName + '-' + video.id, value)
+    }
+  })
+
+  // Add your custom value to the video, so the client autofill your field using the previously stored value
+  registerHook({
+    target: 'filter:api.video.get.result',
+    handler: async (video) => {
+      if (!video) return video
+      if (!video.pluginData) video.pluginData = {}
+
+      const result = await storageManager.getData(fieldName + '-' + video.id)
+      video.pluginData[fieldName] = result
+
+      return video
+    }
+  })
 }
